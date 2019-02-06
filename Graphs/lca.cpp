12c750ee94003d31f5ddd4d2cf72d491c6eb480a
Matheus Leon <mlan@cin.ufpe.br>
2019-02-06 17:46:15 -0300

    Indentation

--- Graphs/lca.cpp
+++ Graphs/lca.cpp
@@ -8,39 +8,39 @@ int tin[MAXN], tout[MAXN], depth[MAXN], up[MAXN][maxl];
 int timer = 0;
 
 void dfs(int u, int last){
-	tin[u] = ++timer;
-	up[u][0] = last;
-	depth[u] = depth[last] + 1;
-	for (int i = 1; i < maxl; i++) {
-		up[u][i] = up[up[u][i-1]][i-1];
-	}
-	for (auto v : adj[u]) {
-		if (v != last) {
-			dfs(v, u);
-		}
-	}
-	tout[u] = ++timer;
+  tin[u] = ++timer;
+  up[u][0] = last;
+  depth[u] = depth[last] + 1;
+  for (int i = 1; i < maxl; i++) {
+    up[u][i] = up[up[u][i-1]][i-1];
+  }
+  for (auto v : adj[u]) {
+    if (v != last) {
+      dfs(v, u);
+    }
+  }
+  tout[u] = ++timer;
 }
 
 bool is_ancestor(int u, int v){
-	if (tin[u] < tin[v] && tout[u] > tout[v]) return true;
-	return false;
+  if (tin[u] < tin[v] && tout[u] > tout[v]) return true;
+  return false;
 }
 
 int lca(int u, int v){
   if (u == v) return u;
-	if (is_ancestor(u, v)) return u;
+  if (is_ancestor(u, v)) return u;
   if (is_ancestor(v, u)) return v;
-	for (int i = maxl - 1; i >= 0; i--){
-		if (!is_ancestor(up[u][i], v)){
-			u = up[u][i];
-		}
-	}
-	return up[u][0];
+  for (int i = maxl - 1; i >= 0; i--){
+    if (!is_ancestor(up[u][i], v)){
+      u = up[u][i];
+    }
+  }
+  return up[u][0];
 }
 
 int dist(int u, int v){
-	return (depth[u] + depth[v]) - 2 * depth[lca(u,v)];
+  return (depth[u] + depth[v]) - 2 * depth[lca(u,v)];
 }
 
 int main() {
